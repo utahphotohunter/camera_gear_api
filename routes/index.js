726a42117a87b78b167d51e506fe5c1f1b59ac4d
utahphotohunter <mitch.hunter1197@gmail.com>
2025-10-18 23:41:37 -0600

    added api documentation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const swagger = require("./swagger")
 
 //=========================================
 // routes
 //=========================================
+router.use("/", swagger);
 router.use("/cameras", require("./cameras"));
 router.use("/lenses", require("./lenses"));
 router.use("/subjects", require("./subjects"));
---
+++ swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "Camera Gear API",
+    description: "API for choosing photography equipment.",
+  },
+  host: "localhost:5500",
+//   host: "cse341-3qth.onrender.com",
+  schemes: ["https"],
+};
+
+const outputFile = "./swagger.json";
+const routes = ["./routes/index.js"];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, routes, doc);
