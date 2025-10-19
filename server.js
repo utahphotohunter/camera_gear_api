9f2eb601fe9a7b628db52a576f05b16847108a07
Mitch Hunter <144734462+utahphotohunter@users.noreply.github.com>
2025-10-18 18:35:02 -0600

    started work on server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// server logic here
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5500;
+const mongoDB = require("./database/connection");
+const bodyParser = require("body-parser");
+
+app
+  .use(cors())
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/", require("./routes"));
+
+mongoDB.initDb((err, mongoDB) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Server listening on https://localhost:${port}`);
+  }
+});
